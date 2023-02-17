acccb1ac2f5a65752bcb885c39d70ff673d8f26c
Mike Scarlet <mike_scarlet@126.com>
2023-02-17 09:25:54 +0800

    stash

--- http_bots/bot_channel_chat/bot_channel_chat_constants.py
+++ http_bots/bot_channel_chat/bot_channel_chat_constants.py
@@ -1,19 +1,6 @@
 
 from SQLiteWrapper import *
 
-from_messages_initiate_dict = {
-  "FromMessages": {
-    "field_definition": {
-      "from_user_id": "INTEGER",
-      "from_message_id": "INTEGER",
-      "media_unique_id": "TEXT"
-    },
-    "primary_keys": ["from_chat_id", "from_message_id"]
-  }
-}
-
-from_messages_table_structure = SQLDatabase.CreateFromDict(from_messages_initiate_dict)
-
 ChatPermissions = [
   kChatPermissionInvalidUser,
   kChatPermissionNormalUser,
@@ -26,3 +13,31 @@ ChatStatuses = [
   kChatStatusActive,
   kChatStatusMute,
 ] = range(3)
+
+user_status_initiate_dict = {
+  "UserStatus": {
+    "field_definition": {
+      "user_id": "INTEGER",
+      "permission": "INTEGER",
+      "status": "INTEGER",
+      "join_time": "REAL",
+      "last_active_time": "REAL",
+    },
+    "primary_keys": ["user_id"]
+  }
+}
+
+user_status_table_structure = SQLDatabase.CreateFromDict(user_status_initiate_dict)
+
+from_messages_initiate_dict = {
+  "FromMessages": {
+    "field_definition": {
+      "from_user_id": "INTEGER",
+      "from_message_id": "INTEGER",
+      "media_unique_id": "TEXT"
+    },
+    "primary_keys": ["from_chat_id", "from_message_id"]
+  }
+}
+
+from_messages_table_structure = SQLDatabase.CreateFromDict(from_messages_initiate_dict)
--- http_bots/bot_channel_chat/bot_channel_chat_main.py
+++ http_bots/bot_channel_chat/bot_channel_chat_main.py
@@ -9,12 +9,22 @@ from utils.async_single_db_auto_commit_serializable_object import *
 from http_bots.bot_channel_chat.bot_channel_chat_constants import *
 import telegram
 import telegram.ext
+import os
 
 class ChannelChatUserStatus:
   def __init__(self) -> None:
+    self.user_id = None
     self.permission = kChatPermissionInvalidUser
     self.status = kChatStatusInactive
+    self.join_time = 0
     self.last_active_time = 0
+
+class UserStatusDatabase(AsyncSingleDBAutoCommitSerializableObject):
+  def __init__(self, db_path: str = None) -> None:
+    super().__init__(db_path, user_status_table_structure)
+
+  async def Initiate(self, loop: asyncio.AbstractEventLoop):
+    await super().Initiate(loop)
 
 class FromMessagesManageDatabase(AsyncSingleDBAutoCommitSerializableObject):
   def __init__(self, db_path: str = None) -> None:
@@ -24,8 +34,13 @@ class FromMessagesManageDatabase(AsyncSingleDBAutoCommitSerializableObject):
     await super().Initiate(loop)
 
 class BotChannelChat:
-  def __init__(self) -> None:
+  def __init__(self, workspace_folder) -> None:
     self._logger = logging.getLogger("BotChannelChat")
+    self._workspace_folder = workspace_folder
+    self._from_message_db = None
+    self._user_status_db = None
+    self._user_info_dict = {}
+    self._forward_process_queue = asyncio.Queue(100)  # max store 100 messages
 
   def PrepareHandlers(self, app: telegram.ext.Application):
     app.add_handler(telegram.ext.CommandHandler("start", self.StartHandler))
@@ -34,9 +49,24 @@ class BotChannelChat:
       )
 
   async def Initiate(self, app: telegram.ext.Application):
+    if self._workspace_folder is None:
+      self._logger.error("workspace folder is None")
+      raise ValueError("workspace folder is None")
+    if not os.path.exists(self._workspace_folder):
+      os.makedirs(self._workspace_folder)
+
     # do all initiate process here
     self._logger.info("initiate is called")
+    
     loop = asyncio.get_event_loop()
+    self._from_message_db = FromMessagesManageDatabase(os.path.join(self._workspace_folder, "from_message.db"))
+    await self._from_message_db.Initiate(loop)
+
+    self._user_status_db = UserStatusDatabase(os.path.join(self._workspace_folder, "user_status.db"))
+    await self._user_status_db.Initiate(loop)
+
+    # TODO: load exist result
+
     self._logger.info("initiate is finished")
 
   async def PrepareStop(self, app: telegram.ext.Application):
@@ -54,7 +84,7 @@ class BotChannelChat:
     pass
   
 def BotChannelChatMain(bot_token):
-  bcc = BotChannelChat()
+  bcc = BotChannelChat("workspace/bot_channel_chat")
 
   app = telegram.ext.ApplicationBuilder().token(bot_token).build()
   bcc.PrepareHandlers(app)
